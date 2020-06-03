c1ac1f71c399d55bbf7a4f15e0a792205c3dddcb
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-03 04:02:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ end
   end
   
   def self.find(name)
-    self.find(name){|artists| artists.name == name}
+    @@all.self.find(name){|artists| artists.name == name}
   end
   
   def self.create(name)

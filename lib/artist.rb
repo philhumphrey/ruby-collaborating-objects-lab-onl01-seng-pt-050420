86e4879f9a71f6960415f66ebc7fcae20b295f1c
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-03 04:23:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,16 @@ end
    @songs << song
   end
   
-  
+    def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
   
   
   

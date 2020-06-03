4e996ff3d763639e5b99ecb72db9181da9c4f283
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-03 06:23:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,8 @@ class Song
   end
   
   def self.new_by_filename(file)
-      artist_name = file.split (" - ")[0]
-      song_name = file.split(" - ")[1]
+      artist = file.split (" - ")[0]
+      song = file.split(" - ")[1]
       new_song = self.new(song)
     new_song.artist_name = artist
     new_song

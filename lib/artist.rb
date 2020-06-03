5ed8fcd2140a23bb40c4c1da7cf465ed911efaf2
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-03 03:51:45 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -40,5 +40,6 @@ end
 #   end
 # end
 
-def self.find_or_create_by_name(artist_name)             
+def self.find_or_create_by_name(artist_name) 
+
 found_artist = self.all.find {|artist| artist.name == artist_name}             if found_artist                 found_artist             else               new_artist = self.new(artist_name)               new_artist.save               new_artist             end           end 
